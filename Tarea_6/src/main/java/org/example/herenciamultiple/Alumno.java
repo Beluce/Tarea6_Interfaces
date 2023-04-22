cdd4c9784cf16332f176db90e933663d4abc9b44
Beluce <112995728+Beluce@users.noreply.github.com>
2023-04-22 14:29:30 -0600

    Add files via upload

---
+++ Tarea_6/src/main/java/org/example/herenciamultiple/Alumno.java
@@ -0,0 +1,60 @@
+package org.example.herenciamultiple;
+
+import javax.xml.crypto.AlgorithmMethod;
+
+public class Alumno extends Persona{
+    private int numCuenta;
+    private String carrera;
+    private double promedio;
+
+    public Alumno() {
+    }
+
+    public Alumno(int edad){
+        super(edad);
+    }
+
+    public Alumno(String nombre, int edad) {
+        super(nombre, edad);
+    }
+
+    public Alumno(String nombre, int edad, int numCuenta, String carrera, double promedio) {
+        super(nombre, edad);
+        this.numCuenta = numCuenta;
+        this.carrera = carrera;
+        this.promedio = promedio;
+    }
+
+    public int getNumCuenta() {
+        return numCuenta;
+    }
+
+    public void setNumCuenta(int numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public double getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(double promedio) {
+        this.promedio = promedio;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" +
+                "numCuenta=" + numCuenta +
+                ", carrera='" + carrera + '\'' +
+                ", promedio=" + promedio +
+                '}';
+    }
+}
